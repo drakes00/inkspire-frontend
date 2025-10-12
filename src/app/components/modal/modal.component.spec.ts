8e197a68ee52c7a8ff09715cdf4f41682091ae5b
Maxime Puys <drakes00@gmail.com>
2025-10-12 16:06:49 +0200

    Added: tests for modal

--- src/app/components/modal/modal.component.spec.ts
+++ src/app/components/modal/modal.component.spec.ts
@@ -1,23 +1,69 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
+import { ModalComponent } from "./modal.component";
+import { SimpleChange } from "@angular/core";
 
-import { ModalComponent } from './modal.component';
+describe("ModalComponent", () => {
+    let component: ModalComponent;
+    let fixture: ComponentFixture<ModalComponent>;
 
-describe('ModalComponent', () => {
-  let component: ModalComponent;
-  let fixture: ComponentFixture<ModalComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FormsModule, ModalComponent],
+        }).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ModalComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe("when showContext is false", () => {
+        it("should not show the context textarea", () => {
+            component.showContext = false;
+            fixture.detectChanges();
+            expect(fixture.nativeElement.textContent).not.toContain("Context (optional):");
+        });
+    });
+
+    describe("when showContext is true", () => {
+        it("should show the context textarea", () => {
+            component.showContext = true;
+            fixture.detectChanges();
+            expect(fixture.nativeElement.textContent).toContain("Context (optional):");
+        });
+    });
+
+    it('should emit "validate" with correct payload', () => {
+        spyOn(component.validate, "emit");
+
+        component.nameValue = "MyNewItem";
+        component.context = "My context";
+        fixture.detectChanges();
+
+        const validateButton = fixture.nativeElement.querySelector("button");
+        validateButton.click();
+
+        expect(component.validate.emit).toHaveBeenCalledWith({
+            name: "MyNewItem",
+            context: "My context",
+        });
+    });
+
+    it("should reset form fields when it becomes visible", () => {
+        component.nameValue = "Old Name";
+        component.context = "Old Context";
+
+        component.isVisible = true;
+        component.ngOnChanges({
+            isVisible: new SimpleChange(false, true, false),
+        });
+        fixture.detectChanges();
+
+        expect(component.nameValue).toBe("");
+        expect(component.context).toBe("");
+    });
 });
