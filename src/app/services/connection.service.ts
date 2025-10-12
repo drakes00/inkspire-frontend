2b0008d5c5ac3fb3916c480f9155964f58384026
Maxime Puys <drakes00@gmail.com>
2025-10-12 12:02:06 +0200

    Formatting

--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -1,64 +1,57 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class ConnectionService {
+    constructor() {}
 
-  constructor() { }
+    public async connectUser(username: string, password: string): Promise<boolean> {
+        const url = "/auth";
+        const contentType = "application/json";
+        const body = JSON.stringify({ username: username, password: password });
 
-  public async connectUser(username: string, password: string): Promise<boolean>
-  {
-    const url = '/auth';
-    const contentType = 'application/json';
-    const body = JSON.stringify({ username: username, password: password });
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                body: body,
+                headers: {
+                    Accept: contentType,
+                    "Content-Type": contentType,
+                },
+            });
 
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: body,
-        headers: {
-          Accept: contentType,
-          'Content-Type': contentType,
-        },
-      });
-
-        console.log("ok")
-      if (response.ok) {
-        const data = await response.json();
-        if (data.token) {
-          localStorage.setItem('token', data.token);
-          return true;
+            if (response.ok) {
+                const data = await response.json();
+                if (data.token) {
+                    localStorage.setItem("token", data.token);
+                    return true;
+                }
+            }
+            return false;
+        } catch (e) {
+            console.error("Connection error", e);
+            return false;
         }
-      }
-      return false;
-    } catch (e) {
-      console.error('Connection error', e);
-      return false;
     }
-  }
 
-  public async disconnectUser(): Promise<void>
-  {
-    const token = localStorage.getItem('token');
-    localStorage.removeItem('token');
+    public async disconnectUser(): Promise<void> {
+        const token = localStorage.getItem("token");
+        localStorage.removeItem("token");
 
-    if (token) {
-      const url = '/logout';
-      try {
-        await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Accept': 'application/json',
-          }
-        });
-      } catch (e) {
-        console.error('Error during logout API call', e);
-      }
+        if (token) {
+            const url = "/logout";
+            try {
+                await fetch(url, {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: "application/json",
+                    },
+                });
+            } catch (e) {
+                console.error("Error during logout API call", e);
+            }
+        }
     }
-  }
-
-
-
 }
